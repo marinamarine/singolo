b1f43af97745cb9abfd6d9aa458158086f11ad32
marinamarine <marinakarpovskaya@gmail.com>
2020-03-16 02:26:16 +0300

    feat: implement form submit message

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+// window.onload = function () {
 const menu = document.querySelector('.menu__inner');
+
+const form = document.querySelector('.form');
+const subject = document.querySelector('#subject');
+const text = document.querySelector('#text');
+
+const message = document.querySelector('.message');
+const closeBtn = document.querySelectorAll('.close');
+const messageSubject = document.querySelector('.message__subject');
+const messageDescription = document.querySelector('.message__description');
+
+function openMessage() {
+  message.classList.remove('hidden');
+  setTimeout(() => message.style.opacity = 1, 400);
+}
+
+function closeMessage() {
+  message.style.opacity = 0;
+  setTimeout(() => message.classList.add('hidden'), 400);
+}
+
+
+
+//menu
 menu.addEventListener('click', (event) => {
   event.preventDefault();
   const targetItem = event.target;
@@ -10,6 +34,35 @@ menu.addEventListener('click', (event) => {
     const element = document.querySelector(targetItem.getAttribute('href'));
     const y = element.getBoundingClientRect().top + window.pageYOffset - yOffset;
 
-    window.scrollTo({top: y, behavior: 'smooth'});
+    window.scrollTo({ top: y, behavior: 'smooth' });
   }
 });
+
+//form popup
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (!subject.value) {
+    messageSubject.innerHTML = 'Without subject';
+  } else {
+    messageSubject.innerHTML = 'Subject: ' + String(subject.value);
+  }
+
+  if (!text.value) {
+    messageDescription.innerHTML = 'Without description';
+  } else {
+    messageDescription.innerHTML = 'Description: ' + String(text.value);
+  }
+
+  form.reset();
+
+  openMessage();
+});
+
+
+
+closeBtn.forEach(item => item.addEventListener('click', closeMessage));
+
+
+
+// }
