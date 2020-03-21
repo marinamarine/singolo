a6e0db2c88fa3835ea30b53eba428991b4f68d00
marinamarine <marinakarpovskaya@gmail.com>
2020-03-21 23:56:14 +0300

    feat: add slider and phone screen manipulation

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // window.onload = function () {
+
+/*  MENU  */
 const menu = document.querySelector('.menu__inner');
 const menuItems = menu.querySelectorAll('.menu__link');
 const menuItemsArray = Array.from(menuItems);
@@ -10,6 +12,18 @@ const scrollItemsArray = menuItemsArray.map((item) =>
 let lastId;
 let currentMenuItem;
 
+/*  SLIDER  */
+const phones = document.querySelectorAll('.iphone');
+
+const slider = document.querySelector('.slider');
+const sliderInner = document.querySelector('.slider__inner');
+const slides = document.querySelectorAll('.slide');
+let currentSlideIndex = 0;
+let isEnable = true;
+const arrowPrev = document.querySelector('.slider__arrow.arrow-prev');
+const arrowNext = document.querySelector('.slider__arrow.arrow-next');
+
+/*  FORM  */
 const form = document.querySelector('.form');
 const subject = document.querySelector('#subject');
 const text = document.querySelector('#text');
@@ -19,6 +33,7 @@ const closeBtn = document.querySelectorAll('.close');
 const messageSubject = document.querySelector('.message__subject');
 const messageDescription = document.querySelector('.message__description');
 
+/*  PORTFOLIO  */
 const tabs = document.querySelector('.tabs');
 const tabsItems = document.querySelectorAll('.tabs__link');
 
@@ -26,22 +41,128 @@ const portfolio = document.querySelector('.portfolio__items');
 const portfolioItems = document.querySelectorAll('.portfolio__item');
 let portfolioItemsArray;
 
-//functions
+
+/*  SLIDER  */
+
+//slider phones screens
+phones.forEach(item => {
+  item.addEventListener('click', function (event) {
+    this.classList.toggle('iphone_active');
+  });
+});
+
+function changeCurrentItem(n) {
+  currentSlideIndex = (n + slides.length) % slides.length;
+}
+
+function hideSlide(direction) {
+  isEnable = false;
+  slides[currentSlideIndex].classList.add(direction);
+  slides[currentSlideIndex].addEventListener('animationend', function () {
+    this.classList.remove('active', direction);
+  })
+}
+function showSlide(direction) {
+  slides[currentSlideIndex].classList.add('next', direction);
+  slides[currentSlideIndex].addEventListener('animationend', function () {
+    this.classList.remove('next', direction);
+    this.classList.add('active');
+    isEnable = true;
+  })
+}
+
+function changeSliderBackground() {
+  slider.classList.toggle('slider_colored');
+}
+
+function previousSlide(n) {
+  hideSlide('to-right');
+  changeCurrentItem(n - 1);
+  changeSliderBackground();
+  showSlide('from-left');
+}
+
+function nextSlide(n) {
+  hideSlide('to-left');
+  changeCurrentItem(n + 1);
+  changeSliderBackground();
+  showSlide('from-right');
+}
+
+
+arrowPrev.addEventListener('click', function () {
+  if (isEnable) {
+    previousSlide(currentSlideIndex);
+  }
+});
+
+arrowNext.addEventListener('click', function () {
+  if (isEnable) {
+    nextSlide(currentSlideIndex);
+  }
+});
+
+//swipe
+const swipedetect = (element) => {
+  let surface = element;
+
+  let startX = 0;
+  let startY = 0;
+
+  let dist = 0;
+  let distX = 0;
+  let distY = 0;
+
+  let startTime = 0;
+  let elapsedTime = 0;
+
+  let thresholdX = 100;
+  let restraintY = 100;
+
+  let allowedTime = 600;
+
+  surface.addEventListener('mousedown', function (event) {
+    dist = 0;
+    startX = event.pageX;
+    startY = event.pageY;
+    startTime = new Date().getTime();
+    // event.preventDefault();
+  });
+
+  surface.addEventListener('mouseup', function (event) {
+    distX = event.pageX - startX;
+    distY = event.pageY - startY;
+    elapsedTime = new Date().getTime() - startTime;
+
+    if (elapsedTime <= allowedTime) {
+      if (Math.abs(distX) >= thresholdX && Math.abs(distY) <= restraintY) {
+        if (distX > 0) {
+          if (isEnable) {
+            previousSlide(currentSlideIndex);
+          }
+        } else {
+          if (isEnable) {
+            nextSlide(currentSlideIndex);
+          }
+        }
+        event.preventDefault();
+      }
+    }
+
+
+
+
+  });
+}
+
+swipedetect(sliderInner);
+
+/*  MENU */
+
 function activateMenuItem(currentItem) {
   menuItems.forEach(item => item.classList.remove('menu__link_active'));
   currentItem.classList.add('menu__link_active');
 }
-
-function openMessage() {
-  message.classList.remove('hidden');
-  setTimeout(() => (message.style.opacity = 1), 400);
-}
-
-function closeMessage() {
-  message.style.opacity = 0;
-  setTimeout(() => message.classList.add('hidden'), 400);
-}
-
 
 //menu to scroll
 menu.addEventListener('click', event => {
@@ -67,14 +188,27 @@ window.addEventListener('scroll', function (event) {
       let id = item.getAttribute('id');
       if (lastId !== id) {
         lastId = id;
-        
+
         menuItems.forEach(item => {
-          item.getAttribute("href") === '#' + id ? currentMenuItem=item : ''});
-          activateMenuItem(currentMenuItem);
+          item.getAttribute("href") === '#' + id ? currentMenuItem = item : ''
+        });
+        activateMenuItem(currentMenuItem);
       }
     }
   });
 });
+
+/*  FORM  */
+
+function openMessage() {
+  message.classList.remove('hidden');
+  setTimeout(() => (message.style.opacity = 1), 400);
+}
+
+function closeMessage() {
+  message.style.opacity = 0;
+  setTimeout(() => message.classList.add('hidden'), 400);
+}
 
 //form popup
 form.addEventListener('submit', event => {
@@ -99,6 +233,8 @@ form.addEventListener('submit', event => {
 
 closeBtn.forEach(item => item.addEventListener('click', closeMessage));
 
+/*  PORTFOLIO */
+
 //portfolio tabs
 
 function filterPortfolio(currentTab) {
@@ -110,12 +246,12 @@ function filterPortfolio(currentTab) {
 
   tabsItems.forEach(item => item.classList.remove('tabs__link_active'));
   currentTab.classList.add('tabs__link_active');
-  
+
   portfolioItemsArray = Array.from(portfolioItems);
-  
-  portfolioItemsArray.map((item, index, array)=>{
-    if (item.dataset.tabitem == currentIndex){
-      array.splice(index,1);
+
+  portfolioItemsArray.map((item, index, array) => {
+    if (item.dataset.tabitem == currentIndex) {
+      array.splice(index, 1);
       array.unshift(item);
     }
   });
@@ -127,7 +263,7 @@ function filterPortfolio(currentTab) {
 tabs.addEventListener('click', event => {
   event.preventDefault();
   filterPortfolio(event.target);
-  
+
 });
 
 //portfolio items active
